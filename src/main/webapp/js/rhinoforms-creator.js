9d0f97fe918380abc1371f950dfd7782a5cb4221
Kai Kewley <kaikewley@gmail.com>
2012-07-25 14:13:09 +0100

    Started form creator

---
+++ src/main/webapp/js/rhinoforms-creator.js
@@ -0,0 +1,87 @@
+function RhinoformsCreator() {
+	
+	this.$container = null;
+	this.$currentFieldDiv = null;
+	this.$fieldEditor = null;
+	
+	this.init = function(container) {
+		this.$container = $(container);
+		this.$fieldEditor = $("#fieldEditor");
+		
+		var rfc = this;
+		$("[name='label']", this.$fieldEditor).bind("input paste", function() {
+			$("label", rfc.$currentFieldDiv).text($(this).val());
+		})
+		$("[name='name']", this.$fieldEditor).bind("input paste", function() {
+			$("input", rfc.$currentFieldDiv).attr("name", $(this).val());
+		})
+		$("[name='validation']", this.$fieldEditor).bind("input paste", function() {
+			$("input", rfc.$currentFieldDiv).attr("validation", $(this).val());
+		})
+		
+		this.$fieldEditor.hover(function() { rfc.overFieldEditor($(this)); }, function() { rfc.outFieldEditor($(this)); });
+	}
+	
+	this.newForm = function() {
+		this.$container.html($("<form>").attr("rhinoforms", "true"));
+	}
+	
+	this.addInput = function() {
+		var $fieldDiv = $("<div>").addClass("rfField");
+		var label = $("<label>").attr("for", "");
+		var input = $("<input>").attr("name", "").attr("type", "text");
+		$fieldDiv.append(label).append("<br/>").append(input);
+		this.setLabel("New Field", $fieldDiv);
+		this.setName("newField", $fieldDiv);
+		var rfc = this;
+		$("form", this.$container).append($fieldDiv);
+		$(".rfField", this.$container).hover(function() { rfc.overField($(this)); }, function() { rfc.outField($(this)); });
+	}
+	
+	this.setName = function(name, fieldDiv) {
+		$("label", fieldDiv).attr("for", name);
+		$("input", fieldDiv).attr("name", name);
+	}
+
+	this.setLabel = function(label, fieldDiv) {
+		$("label", fieldDiv).text(label);
+	}
+	
+	this.overField = function($fieldDiv) {
+		this.$currentFieldDiv = $fieldDiv;
+		this.$currentFieldDiv.overField = true;
+		
+		$(".rfField", this.$container).removeClass("rfFieldOver");
+		$fieldDiv.addClass("rfFieldOver");
+
+		$("[name='label']", this.$fieldEditor).val($("label", this.$currentFieldDiv).text());
+		$("[name='name']", this.$fieldEditor).val($("input", this.$currentFieldDiv).attr("name"));
+		$("[name='validation']", this.$fieldEditor).val($("input", this.$currentFieldDiv).attr("validation"));
+		this.$fieldEditor.show().css("top", $fieldDiv[0].offsetTop);
+	}
+	
+	this.outField = function($fieldDiv) {
+		this.$currentFieldDiv.overField = false;
+		var rfc = this;
+		setTimeout(function() {rfc.checkFieldOut()}, 100);
+	}
+	
+	this.overFieldEditor = function() {
+		this.$currentFieldDiv.overFieldEditor = true;
+	}
+
+	this.outFieldEditor = function() {
+		this.$currentFieldDiv.overFieldEditor = false;
+		var rfc = this;
+		setTimeout(function() {rfc.checkFieldOut()}, 100);
+	}
+	
+	this.checkFieldOut = function() {
+		if (this.$currentFieldDiv != null && this.$currentFieldDiv.overField != true && this.$currentFieldDiv.overFieldEditor != true ) {
+			this.$currentFieldDiv.removeClass("rfFieldOver");
+			this.$fieldEditor.hide();
+			this.$currentFieldDiv = null;
+		}
+	}
+	
+}
