4881839c2ec0f03af4f6fbe7c1a02da32c11978e
Kai Kewley <kaikewley@gmail.com>
2012-07-25 08:16:20 +0100

    Added invalid field highlighting on submit.

--- src/main/webapp/js/rhinoforms.js
+++ src/main/webapp/js/rhinoforms.js
@@ -1,13 +1,16 @@
 function Rhinoforms() {
 	
 	this.loadForm = function(formPath, $container) {
-		$container.html("Loading " + formPath);
-		
+		var rf = this;
 		$.ajax({
 			url: formPath,
 			data: { rhinoforms: "true", random: Math.random() },
 			success: function(html) {
 				$container.html(html);
+				$("form", $container).submit(function() {
+					rf.onSubmit(this);
+					return false;
+				});
 			},
 			failure: function() {
 				alert("Failed to load form");
@@ -16,13 +19,28 @@ function Rhinoforms() {
 		
 	}
 	
+	this.onSubmit = function(form) {
+		if (this.validateForm(form) == true) {
+			$.ajax({
+				url: "form",
+				data: $form.serialize(),
+				success: function(data) {
+					$form.html(data);
+				}
+			});
+		} else {
+			alert("Not valid");
+		}
+	}
+	
 	this.validateForm = function(form) {
 		var simpleForm = this;
 		var submit = true;
+		var $form = $(form);
 		
 		// Compile map of fields with their values and validation options
 		var fields = {};
-		$(":input", form).each(function() {
+		$(":input", $form).each(function() {
 			var $input = $(this);
 			var name = $input.attr("name")
 			var type = $input.attr("type")
@@ -44,11 +62,10 @@ function Rhinoforms() {
 		// Build error list
 		var $errorList = $("<ul>").addClass("rfError");
 		for (var a in errors) {
-			$errorList.append($("<li>").html(errors[a]));
+			$errorList.append($("<li>").attr("name", errors[a].name).html(errors[a].message));
 		}
 		
 		// Attach error list to dom
-		var $form = $(form);
 		var prev = $form.prev();
 		if (prev.hasClass("rfError")) {
 			prev.html($errorList.html());
@@ -56,11 +73,18 @@ function Rhinoforms() {
 			$form.before($errorList);
 		}
 		
+		// Add invalid class to inputs
+		$("input", $form).removeClass("invalid");
+		for (var a in errors) {
+			$("input[name='" + errors[a].name + "']", $form).addClass("invalid");
+		}
+		
 		// return true if no errors, otherwise false
 		return errors.length == 0;
 	}
 	
-	// Take a map of fields and validate each returning a list of any errors
+	// Take a map of fields and validate each returning a list of any errors.
+	// This is also run server-side
 	this.validateFields = function(fields) {
 		var simpleForm = this;
 		var errors = [];
@@ -89,15 +113,19 @@ function Rhinoforms() {
 		var validationArray = validationList.split(" ");
 		for (a in validationArray) {
 			var validation = validationArray[a];
+			var message = null;
 			if (validation == "required") {
 				if (value.length == 0) {
-					return name + " is required.";
+					message = name + " is required.";
 				}
 			} else if (validation == "email") {
 				var result = this.validateEmail(value);
 				if (result != true) {
-					return name + " is not a valid email address.";
+					message = name + " is not a valid email address.";
 				}
+			}
+			if (message) {
+				return { name: name, message: message };
 			}
 		}
 		return null;
@@ -114,5 +142,9 @@ function Rhinoforms() {
 	this.validateEmail = function(email) {
 		var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 		return reg.test(email);
+	}
+	
+	this.trace = function(message) {
+		$("#trace").append(message);
 	}
 }
