ccea5a2aebdc6d5f104adb5aeb280c76da21b9bf
Kai Kewley <kaikewley@gmail.com>
2013-03-27 11:52:13 +0000

    Feature: Submission URLs with HttpAuth.
    Uses format: http://user:pass@hostname/...

--- src/main/java/com/rhinoforms/net/ConnectionFactoryImpl.java
+++ src/main/java/com/rhinoforms/net/ConnectionFactoryImpl.java
@@ -5,11 +5,27 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.apache.commons.codec.binary.Base64;
+
 public class ConnectionFactoryImpl implements ConnectionFactory {
 
 	@Override
 	public HttpURLConnection openConnection(String url) throws MalformedURLException, IOException {
-		return (HttpURLConnection) new URL(url).openConnection();
+		HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+		
+		// Add Auth
+		if (url.contains("@")) {
+			// Format: http://user:pass@hostname/
+			String usernamePassword = url.substring(url.indexOf("//"), url.lastIndexOf("@"));
+			String[] usernamePasswordParts = usernamePassword.split(":");
+			connection.setRequestProperty("Authorization", "Basic " + base64Encode(usernamePasswordParts[0] + ":" + usernamePasswordParts[1]));
+		}
+		
+		return connection;
+	}
+	
+	private static String base64Encode(String source) {
+		return new String(new Base64().encode(source.getBytes())).trim();
 	}
 
 }
